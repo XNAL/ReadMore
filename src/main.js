3ac98c0bcec84b637392f32cf6ae0105d3f64e7a
XNAL <772528797@qq.com>
2017-07-17 18:45:24 +0800

    使用fastclick处理click事件延迟问题

--- src/main.js
+++ src/main.js
@@ -4,6 +4,13 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store/'
+import FastClick from 'fastclick'
+
+if ('addEventListener' in document) {
+    document.addEventListener('DOMContentLoaded', function() {
+        FastClick.attach(document.body);
+    }, false);
+}
 
 Vue.config.productionTip = false
 
